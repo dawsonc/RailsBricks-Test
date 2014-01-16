22832be896a6a399d9d33ff78a3363ebccb2f94b
TheBritKnight <charles.dwsn@gmail.com>
2014-01-16 18:16:41 -0500

    initial commit (generated by RailsBricks)

---
+++ RailsBricks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBricks::Application.config.secret_key_base = 'c40411473458c349fef4d500797ba702e8ee5664a825dcdc911c2a76477a175421ac3d3d066dfa18afc2166e2e738c55d6e1f2c07faa8a872e97d4a698bf2a9a'
---
+++ RailsBricks/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBricks::Application.config.session_store :cookie_store, key: '_RailsBricks_session'
